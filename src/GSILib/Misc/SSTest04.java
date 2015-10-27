b9aa4077f5c2ea3b18c3841222504d731a6448e8
Labora1 <Labora1@A01341W.aul.a.unavarra.es>
2015-10-27 09:41:46 +0100

    Se han creado exibiciones y mas localizaciones en SSTest04

--- src/GSILib/Misc/SSTest04.java
+++ src/GSILib/Misc/SSTest04.java
@@ -9,6 +9,7 @@ package GSILib.Misc;
 import GSILabs.BModel.Artist;
 import GSILabs.BModel.Collective;
 import GSILabs.BModel.Concert;
+import GSILabs.BModel.Exhibition;
 import GSILabs.BModel.Festival;
 import GSILabs.BModel.Location;
 import GSILabs.BSystem.BusinessSystem;
@@ -23,12 +24,16 @@ public class SSTest04 {
     
     public static void main (String[] args) throws IOException {
     
-        Artist a1,a2,a3,a4,a5,a6,a7,a8;
+        Artist a1,a2,a3,a4,a5,a6,a7,a8,a9,a10;
         Collective c1,c2;
         Concert con1,con2,con3,con4;
-        Location l1,l2;
+        Location l1,l2,l3,l4;
+        Exhibition e1,e2;
         Festival f1;
         Date d1,d2,d3,d4;
+        Date[] d5 = null;
+        String[] webs1={"www.museoreinasofia.es","www.fundacionpinturaparatodos.es"};
+        String[] webs2={"www.renacimeintohoy.es","www.arteclasicopamplona.es"};
         BusinessSystem miBs=new BusinessSystem();
 
         
@@ -40,6 +45,8 @@ public class SSTest04 {
         a6=new Artist("Pete Wentz","Guitarrista y bajista");
         a7=new Artist("Jared Leto","Cantante y actor");
         a8=new Artist("Steve Aoki","Productor de música","www.steveaoki.com");
+        a9=new Artist("Pablo Picasso","Pintor");
+        a10=new Artist("Michelangelo","Pintor,escultor y arquitecto");
         
         c1=new Collective("Alex y los rebujitos",a2,a3,"www.alexylosrebujitos.es");
         c1.addComponent(a4);
@@ -48,6 +55,8 @@ public class SSTest04 {
         
         miBs.addArtist(a1);
         miBs.addArtist(a7);
+        miBs.addArtist(a9);
+        miBs.addArtist(a10);
         miBs.addCollective(c1);
         miBs.addCollective(c2);
         
@@ -55,6 +64,10 @@ public class SSTest04 {
         miBs.addLocation(l1);
         l2=new Location("Lizarreria",3000,"España","Navarra","Estella","Calle Mayor",(short) 1);
         miBs.addLocation(l2);
+        l3=new Location("Museo Reina Sofia",5000,"España","Madrid","Madrid","Paseo de la castellana",(short) 23);
+        miBs.addLocation(l3);
+        l4=new Location("Museo de arte clasico de pamplona",3000,"España","Navarra","Pamplona","Calle La estafeta",(short)3);
+        miBs.addLocation(l4);
 
         d1=new Date(2016-1900,21,2);
         d2=new Date(2016-1900,30,3);
@@ -66,6 +79,16 @@ public class SSTest04 {
 
         f1=new Festival();
         miBs.addConcertToFestival(f1, con2);
+        
+        d3=new Date(2016-1900,21,2);
+        d4=new Date(2016-1900,21,3);
+        d5[1]=d3;
+        d5[2]=d4;
+        
+        e1 = new Exhibition("Exposicion de Pablo Picasso","Fundación pintura para todos",d5,l3,a9,webs1);
+        miBs.addNewExhibition(e1);
+        e2 = new Exhibition("Miguel Angel: su historia","Renacimiento hoy",d5,l4,a10,webs2);
+        miBs.addNewExhibition(e2);
         
 
     }
