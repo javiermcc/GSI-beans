0531436b00b1cf01dbff73cfee68fbb2eac1ecbd
Jub3r <>
2015-11-12 11:50:57 +0100

    xml ticket hecho falta used

--- src/GSILabs/BModel/Ticket.java
+++ src/GSILabs/BModel/Ticket.java
@@ -8,7 +8,22 @@ package GSILabs.BModel;
 
 import GSILabs.serializable.XMLRepresentable;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  *
@@ -231,16 +246,124 @@ public class Ticket implements Comparable, XMLRepresentable{
 
     @Override
     public String toXML() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    try {
+
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+        // root elements (ticket)
+        Document doc = docBuilder.newDocument();
+        Element rootElement = doc.createElement("ticket");
+        doc.appendChild(rootElement);
+
+        // id elements
+        Element xId = doc.createElement("id");
+        xId.appendChild(doc.createTextNode(Integer.toString(this.getId())));
+        rootElement.appendChild(xId);
+        
+        // event elements
+        Element xEvent = doc.createElement("event");
+        xEvent.appendChild(doc.createTextNode("##E##"));
+        rootElement.appendChild(xEvent);
+        
+        // associated elements
+        Element xAssociated = doc.createElement("associated");
+        xAssociated.appendChild(doc.createTextNode("##A##"));
+        rootElement.appendChild(xAssociated);
+        
+        // identifiers elements
+        Element xIdentifiers = doc.createElement("identifiers");
+        rootElement.appendChild(xIdentifiers);
+        int[] i = this.getIDs();
+        for (int j = 0; j < i.length; j++){
+            Element xIdentifier = doc.createElement("identifier");
+            xIdentifier.appendChild(doc.createTextNode(Integer.toString(i[j])));
+            xIdentifiers.appendChild(xIdentifier);
+        }
+
+        // used elements
+        Element xUsed = doc.createElement("used");
+        xUsed.appendChild(doc.createTextNode("chorrada"));
+        rootElement.appendChild(xUsed);
+
+        
+        // write the content into string
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(doc);
+
+        StringWriter outWriter = new StringWriter();
+        StreamResult result = new StreamResult( outWriter );
+
+        transformer.transform(source, result);
+        StringBuffer sb = outWriter.getBuffer(); 
+        String finalstring = sb.toString();
+        
+        String c = this.getAssociated().toXML();
+        String c2 = c.replace("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><client>", "");
+        String c3 = c2.replace("</client>", "");
+        finalstring = finalstring.replace("##A##",c3);
+
+
+        Event e = this.getEvent();
+        String e3 = null;
+        if (e instanceof Concert){
+            Concert e2 = (Concert)e;
+            e3 = e2.toXML();
+        }
+        if (e instanceof Exhibition){
+            Exhibition e2 = (Exhibition)e;
+            e3 = e2.toXML();
+        }
+        if (e instanceof Festival){
+            Festival e2 = (Festival)e;
+            e3 = e2.toXML();
+        }
+        String e4 = e3.replace("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>", "");
+        finalstring = finalstring.replace("##E##", e4);
+        
+       
+        return finalstring;
+        
+
+        } catch (ParserConfigurationException pce) {
+              pce.printStackTrace();
+        } catch (TransformerException tfe) {
+              tfe.printStackTrace();
+        }
+        return null;
     }
 
     @Override
     public boolean saveToXML(File f) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        String toXML = this.toXML();
+
+        try {
+            PrintWriter out = new PrintWriter(f);
+            out.write(toXML);
+            out.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Artist.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return true;
     }
 
     @Override
     public boolean saveToXML(String filePath) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String toXML = this.toXML();
+       
+        try { 
+            File f = new File(filePath);
+            PrintWriter out = new PrintWriter(f);
+            out.write(toXML);
+            out.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Artist.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return true;
     }
 }
